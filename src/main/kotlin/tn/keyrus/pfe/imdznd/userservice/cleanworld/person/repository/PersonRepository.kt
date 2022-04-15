7597dac29ddc5c367036168fdd0440c48634799f
Imed ZnD <imedzenned@yahoo.fr>
2022-04-15 23:56:12 +0100

    person service and person service test

--- src/main/kotlin/tn/keyrus/pfe/imdznd/userservice/cleanworld/person/repository/PersonRepository.kt
+++ src/main/kotlin/tn/keyrus/pfe/imdznd/userservice/cleanworld/person/repository/PersonRepository.kt
@@ -2,7 +2,6 @@ package tn.keyrus.pfe.imdznd.userservice.cleanworld.person.repository
 
 import io.vavr.control.Either
 import kotlinx.coroutines.flow.Flow
-import reactor.core.publisher.Mono
 import tn.keyrus.pfe.imdznd.userservice.cleanworld.person.model.Person
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -23,7 +22,6 @@ interface PersonRepository {
     fun findAllPersonByNumberOfFlagsGreaterThan(numberOfFlags: Int): Flow<Person>
     fun findAllPersonByNumberOfFlagsLessThan(numberOfFlags: Int): Flow<Person>
     fun findAllPersonByIsFraudster(isFraudster: Boolean): Flow<Person>
-
     fun findAllPersonByBirthYearAndCountryCode(year: Year,countryCode: String): Flow<Person>
     fun findAllPersonByBirthYearAndHasEmail(birthYear: Year, hasEmail: Boolean): Flow<Person>
     fun findAllPersonByBirthYearBefore(birthYear: Year): Flow<Person>
@@ -37,11 +35,6 @@ interface PersonRepository {
     fun findAllPersonByFailedSignInAttemptsGreaterThan(failedSignInAttempts: Int): Flow<Person>
     fun findAllPersonByFailedSignInAttemptsLessThan(failedSignInAttempts: Int): Flow<Person>
     fun findAllPersonByFraudsterAndCountryCode(isFraudster: Boolean, country: String): Flow<Person>
-//    @Query(
-//        """select count(p.id)
-//                from person p
-//                where p.birth_year = :1"""
-//    )
     suspend fun countByBirthYear(birthYear: Year): Long
     suspend fun countAllPersonByState(state: Person.PersonState): Long
     suspend fun countAllPersonByTermsVersion(termsVersion: LocalDate): Long
